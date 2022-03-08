4fb22417b9d8dfbaa069fda8a16a974b88138423
coldhailstone <cksn1993@gmail.com>
2022-03-08 14:45:31 +0900

    넉스트의 라우팅 관련 컴포넌트 정리

--- pages/main.vue
+++ pages/main.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
+      <nuxt-logo></nuxt-logo>
       <h1>메인 페이지</h1>
   </div>
 </template>
 
 <script>
-export default {
+import NuxtLogo from '../components/NuxtLogo.vue';
 
+export default {
+    components: {
+        NuxtLogo
+    }
 }
 </script>
 
