9a476ddab88c99741c48468cbd7415500c7741e4
coldhailstone <cksn1993@gmail.com>
2022-03-10 15:06:48 +0900

    상품 목록 표시 기능 구현

--- pages/main.vue
+++ pages/main.vue
@@ -1,26 +1,31 @@
 <template>
   <div>
     <p>메인 페이지 입니다.</p>
-    <ProductList>
-
-    </ProductList>
+    <div>
+      <ul>
+        <li v-for="product in products" :key="product.id">
+          <img :src="product.imageUrl" :alt="product.name" />
+          <p>{{ product.name }}</p>
+          <p>{{ product.price }}</p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-// import axios from 'axios';
-import ProductList from '../components/ProductList.vue';
+import axios from 'axios';
 
 export default {
   name: 'MainPage',
-  components: {
-    ProductList
+  async asyncData() {
+    const response = await axios.get('http://localhost:3000/products');
+    const products = response.data.map(item => ({
+      ...item,
+      imageUrl: `${item.imageUrl}?random=${Math.random()}`
+    }));
+    return { products };
   },
-  // async asyncData() {
-  //   const response = await axios.get('http://localhost:3000/products');
-  //   const products = response.data;
-  //   return { products }
-  // },
   data() {
     return {
       // products: []
