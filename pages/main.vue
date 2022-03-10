cc242acaba9ad80575e660203e62d31265ead4ba
coldhailstone <cksn1993@gmail.com>
2022-03-10 14:26:39 +0900

    서버 포트 변경 및 받아온 데이터 표시

--- pages/main.vue
+++ pages/main.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <p>메인 페이지 입니다.</p>
+    <div>
+      {{ products }}
+    </div>
   </div>
 </template>
 
@@ -9,9 +12,14 @@ import axios from 'axios';
 
 export default {
   name: 'MainPage',
+  data() {
+    return {
+      products: []
+    }
+  },
   async created() {
     const response = await axios.get('http://localhost:3000/products');
-    console.log(response);
+    this.products = response.data;
   }
 }
 </script>
