c3df4b148f8d39462849b4a088e47d424bc2a40f
coldhailstone <cksn1993@gmail.com>
2022-03-11 17:17:04 +0900

    fetch 속성 소개와 적용

--- components/CartList.vue
+++ components/CartList.vue
@@ -16,8 +16,8 @@
 import { FETCH_CART_ITEMS } from '@/store';
 
 export default {
-  async asyncData({ store }) {
-    await store.dispatch(FETCH_CART_ITEMS);
+  async fetch() {
+    await this.$store.dispatch(FETCH_CART_ITEMS);
   }
 }
 </script>
