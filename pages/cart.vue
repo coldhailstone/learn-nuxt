3170df6e7491ce0b05ba3e11d922f524ecb447a3
coldhailstone <cksn1993@gmail.com>
2022-03-11 14:17:16 +0900

    장바구니 페이지의 장바구니 목록 조회 기능 구현

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,10 +1,20 @@
 <template>
-  <div>카트 페이지</div>
+  <div>
+    <h1>카트 페이지</h1>
+    <div>
+      <ul>
+        <li v-for="cartItem in $store.state.cartItems" :key="cartItem.id">
+          <img :src="cartItem.imageUrl" :alt="cartItem.name">
+          <p>{{ cartItem.name }}</p>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  name: 'CartPage'
 }
 </script>
 
