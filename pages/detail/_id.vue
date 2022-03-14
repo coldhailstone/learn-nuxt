f81e9a2b1a94a90fd329df1af482b8f435cbe698
coldhailstone <cksn1993@gmail.com>
2022-03-14 17:27:28 +0900

    페이지별 메타 태그 설정 및 확인

--- pages/detail/_id.vue
+++ pages/detail/_id.vue
@@ -26,6 +26,22 @@ export default {
     const product = response.data;
     return { product };
   },
+  head: {
+    title: 'Shopping Item Detail',
+    meta: [
+      {
+        hid: 'description',
+        name: 'description',
+        content: '이 상품은 ~~입니다.',
+      },
+    ],
+    link: [
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Roboto&display=swap'
+      }
+    ]
+  },
   methods: {
     async addToCart() {
       await createCartItem(this.product);
